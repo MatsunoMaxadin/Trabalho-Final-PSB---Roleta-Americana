c2b054ab7c40ac33be785c42b867c094eeb90d62
rafaelbmendonca <rafaelbmendonca0@gmail.com>
2026-06-07 18:38:59 -0300

    Refactor Roletar.asm for improved clarity and functionality

--- src/Roletar.asm
+++ src/Roletar.asm
@@ -1,29 +1,23 @@
-.ORG 0x030
+.ORG 0x070
 
 Roleta:
-	CPI flagLoop, 1
-	BRNE final_loop
+    SEI  ; habilita interrupção
+	CPI flagLoop, 1 ; Verifica se a flag para o loop está ativa.
+	BRNE final_loop ; Se não, encerra o loop.
 	
-	CPI resultado, 0X26
-	BRNE incrementa_resultado
-	LDI resultado, 0x00
-	RJMP Decod
+	CPI resultado, 0X26 ; Confere se o resultado chegou no valor máximo (37).
+	BRNE incrementa_resultado ; Se não, incrementa
+	LDI resultado, 0x00 ; Se sim, Zera o valor
+	RJMP Decod ; Pula para a rotina de decodificação
 	incrementa_resultado:
-	INC resultado
+	INC resultado ; incrementando o resultado
 	Decod:
-	RCALL Mostrar_Display
-	RCALL Atraso
-	RJMP Roleta
+	RCALL Mostrar_Display ; Imprime a animação da roleta girando nos displays
+	RCALL Atraso ; Chama um atraso para a multiplexação ocorrer
+	RJMP Roleta ; Retorna ao início do loop
 	
 
-Atraso:
-	LDI AUX, 255
-	MOV R9, AUX
 
-loop:
-	DEC R9
-	BRNE loop
-	RET
 final_loop:
-	RET
-
+    CLI ; desabilita interrupção
+	RET ; Retorna para a rotina chamadora
